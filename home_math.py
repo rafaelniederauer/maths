8a56607c99849975938da4252c72faf04d6f41eb
rafaelniederauer <rafaelmat@yahoo.com>
2024-06-30 23:25:47 +0100

    Update home_math.py

--- home_math.py
+++ home_math.py
@@ -5,7 +5,7 @@ st.write("Addition")
 st.write("Subtraction")
 st.write("Multiplication")
 st.page_link("pages/1 multiplication_table.py", label="Multiplication table")
-st.page_link("pages/1 multiply_11.py", label="Multiplication by 11")
+st.page_link("pages/2 multiply_11.py", label="Multiplication by 11")
 st.write("Division")
 
 
