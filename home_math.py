8cd37d6cac81ec1e82d931b9348d6131ad35848c
rafaelniederauer <rafaelmat@yahoo.com>
2024-06-30 23:24:35 +0100

    Update home_math.py

--- home_math.py
+++ home_math.py
@@ -4,6 +4,8 @@ st.title("Maths Genius App")
 st.write("Addition")
 st.write("Subtraction")
 st.write("Multiplication")
+st.page_link("pages/1 multiplication_table.py", label="Multiplication table")
+st.page_link("pages/1 multiply_11.py", label="Multiplication by 11")
 st.write("Division")
 
 
